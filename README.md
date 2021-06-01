265b830a9e17769ef09594a894d65a94ec79422a
DP1IZeibe <>
2021-06-01 11:20:28 +0000

    Pārnests projekts uz flask

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lidosta
+**DP1-1**  
+**Izveidoja: Ivars Žeibe, Ivars Levāns**
+
+Mājaslapa izveidota mācību nolūkiem.
+
+Figma: https://www.figma.com/file/DaMXaG8CJebOmHy84GbxyH/Lidosta-team-library?node-id=0%3A1<br/>
+Mājaslapa: https://ivarszeibe.github.io/Lidosta/
--- main.py
+++ main.py
@@ -7,9 +7,29 @@ def index():
   return render_template("index.html")
 
 
-@app.route('/about')
-def about():
-  return render_template("about.html")
+# @app.route('/about')
+# def about():
+#   return render_template("about.html")
+
+@app.route('/administrator')
+def administrator():
+  return render_template("administrator.html")
+
+@app.route('/statistics')
+def statistics():
+  return render_template("statistics.html")
+  
+@app.route('/myTickets')
+def myTickets():
+  return render_template("myTickets.html")
+  
+@app.route('/manageFlights')
+def manageFlights():
+  return render_template("manageFlights.html")
+  
+@app.route('/manageAirports')
+def manageAirports():
+  return render_template("manageAirports.html")
 
 
 app.run(host='0.0.0.0', port=8080)
